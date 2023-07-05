1fb84eefc7f1acc2125b74568ed1cc52a37c0536
Inam <inamulhaque952@gmail.com>
2023-07-05 13:39:32 +0530

    main

--- src/components/AppTitle.jsx
+++ src/components/AppTitle.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export default function AppTitle(props) {
   const {
-    title = 'Box Office',
+    title = 'Movie Wizard',
     subtitle = 'Are you looking for a movie or an actor?',
   } = props;
 
@@ -17,14 +17,25 @@ export default function AppTitle(props) {
 const TitleWrapper = styled.div`
   text-align: center;
   margin: 0 0 40px;
+
   h1 {
     color: ${({ theme }) => theme.mainColors.blue};
-    letter-spacing: 10px;
+    font-size: 3rem;
+    letter-spacing: 5px;
     text-transform: uppercase;
     margin: 0 0 10px;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+    transition: transform 0.2s ease-in-out;
+
+    &:hover {
+      transform: scale(1.1);
+    }
   }
+
   p {
     color: ${({ theme }) => theme.mainColors.dark};
+    font-size: 1.5rem;
     margin: 0;
+    opacity: 0.7;
   }
 `;
